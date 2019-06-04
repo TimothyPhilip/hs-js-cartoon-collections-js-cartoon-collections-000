d257d6fcbc7ec90678ee6abb540f231389c04458
salubrious-reference-8531 <philippetimothe@gmail.com>
2019-06-04 14:31:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function dwarfRollCall(dwarves) {
   var array = []
-  for (var i = dwarves.length/2; i < dwarves.length; i++){
+  for (var i = 0; i < dwarves.length; i++){
      array.push(`${i+1}`+ ". " + dwarves[i] + " ")}
 return array.join("")
 }
@@ -36,12 +36,5 @@ function findTheCheese (foods) {
   
 }
 
-function wordswithb(strings){
-  for (var i = 0, i < strings.length, i++ ){
-    if (strings.chartAt(i) === ){
-      str
-    }
-  }
-  
-}
+
 
